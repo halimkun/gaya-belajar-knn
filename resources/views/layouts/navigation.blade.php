a2cdb7386527d1b17269d627decc89fef8446962
Faisal Halim <ffaisalhalim@gmail.com>
2024-09-09 19:53:53 +0700

    🧩 natigation : role view navigations menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto px-4 sm:px-6 lg:px-8"> <!-- INFO : default width class max-w-7xl -->
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -15,6 +15,19 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    @if (Auth()->user()->hasRole('guru'))
+                        @foreach (['questions', 'educational contents', 'learning styles'] as $item)
+                            <x-nav-link :href="route(Str::replace(' ', '-', $item) . '.index')" :active="request()->routeIs(Str::replace(' ', '-', $item) . '.index')">
+                                {{ __(ucfirst($item)) }}
+                            </x-nav-link>
+                        @endforeach
+                    @endif
+
+                    {{-- TODO : set menu siswa --}}
+                    @if (Auth()->user()->hasRole('siswa'))
+                        
+                    @endif
                 </div>
             </div>
 
