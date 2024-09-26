01929eb6df47a9b7e4c43a068d716b714b2b9097
Faisal Halim <ffaisalhalim@gmail.com>
2024-09-26 23:56:35 +0700

    :hammer: update (dashboard): enhance assessment section to check user detail completion before displaying assessment prompts

--- resources/views/siswa/dashboard.blade.php
+++ resources/views/siswa/dashboard.blade.php
@@ -22,29 +22,32 @@
                         </div>
                     @endif
                     <div class="my-4 border-b border-gray-300 dark:border-gray-700"></div>
-                    @if (Auth::user()->assessments->count() == 0)
-                        {{-- anda belum mengisi assesment, silahkan mengisi terlibih dahulu --}}
-                        <div class="flex items-center justify-between">
-                            <div>
-                                <h1 class="text-lg font-bold">Isi Asesmen Gaya Belajar Anda</h1>
-                                <p class="text-sm text-gray-400">Silahkan isi asesmen karakter anda terlebih dahulu sebelum melanjutkan.</p>
-                            </div>
-                            <a href="{{ route('assessments.create') }}" class="rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white hover:bg-indigo-700">Isi Asesmen</a>
-                        </div>
 
-                        <div class="mt-4 max-w-2xl text-sm text-gray-400">
-                            <p>Asesmen ini bertujuan untuk mengetahui <span class="font-bold text-indigo-500 dark:text-indigo-200">gaya belajar anda</span>. Dengan memahami gaya belajar anda, guru dapat menyesuaikan metode pembelajaran yang paling sesuai, sehingga diharapkan anda dapat belajar dengan lebih efektif dan efisien. Selain itu, dengan mengisi asesmen ini, anda juga akan <span class="font-semibold text-orange-400 dark:text-orange-200">mendapatkan konten rekomendasi dari guru untuk belajar tambahan</span> .</p>
-                        </div>
-                    @else
-                        {{-- anda sudah mengisi assesment, silahkan melihat hasil assesment --}}
-                        <div class="flex items-center justify-between">
-                            <div>
-                                <h1 class="text-lg font-bold">Hasil Asesmen Gaya Belajar Anda</h1>
-                                <p class="text-sm text-gray-400">Anda telah mengisi asesmen karakter anda.</p>
+                    @if (Auth::user()->isDetailComplete())
+                        @if (Auth::user()->assessments->count() == 0)
+                            {{-- anda belum mengisi assesment, silahkan mengisi terlibih dahulu --}}
+                            <div class="flex items-center justify-between">
+                                <div>
+                                    <h1 class="text-lg font-bold">Isi Asesmen Gaya Belajar Anda</h1>
+                                    <p class="text-sm text-gray-400">Silahkan isi asesmen karakter anda terlebih dahulu sebelum melanjutkan.</p>
+                                </div>
+                                <a href="{{ route('assessments.create') }}" class="rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white hover:bg-indigo-700">Isi Asesmen</a>
                             </div>
-                            {{-- add order by created_at --}}
-                            <a href="{{ route('assessments.show', Auth::user()->assessments->sortByDesc('created_at')->first()) }}" class="rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white hover:bg-indigo-700">Lihat Hasil Asesmen</a>
-                        </div>
+
+                            <div class="mt-4 max-w-2xl text-sm text-gray-400">
+                                <p>Asesmen ini bertujuan untuk mengetahui <span class="font-bold text-indigo-500 dark:text-indigo-200">gaya belajar anda</span>. Dengan memahami gaya belajar anda, guru dapat menyesuaikan metode pembelajaran yang paling sesuai, sehingga diharapkan anda dapat belajar dengan lebih efektif dan efisien. Selain itu, dengan mengisi asesmen ini, anda juga akan <span class="font-semibold text-orange-400 dark:text-orange-200">mendapatkan konten rekomendasi dari guru untuk belajar tambahan</span> .</p>
+                            </div>
+                        @else
+                            {{-- anda sudah mengisi assesment, silahkan melihat hasil assesment --}}
+                            <div class="flex items-center justify-between">
+                                <div>
+                                    <h1 class="text-lg font-bold">Hasil Asesmen Gaya Belajar Anda</h1>
+                                    <p class="text-sm text-gray-400">Anda telah mengisi asesmen karakter anda.</p>
+                                </div>
+                                {{-- add order by created_at --}}
+                                <a href="{{ route('assessments.show', Auth::user()->assessments->sortByDesc('created_at')->first()) }}" class="rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white hover:bg-indigo-700">Lihat Hasil Asesmen</a>
+                            </div>
+                        @endif
                     @endif
                 </div>
             </div>
